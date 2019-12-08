59d243462378432d836c3a38a1a4204aec8f2c07
Justin Huang <justin.huang.sz@gmail.com>
2019-12-08 11:01:42 +0800

    Support gunicorn.

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from apis import api
 
-app = Flask(__name__)
-api.init_app(app)
 
-app.run(host="0.0.0.0", port=8080, debug=True)
+def create_app():
+    app = Flask(__name__)
+    api.init_app(app)
+    return app
+
+
+if __name__ == '__main__':
+    application = create_app()
+    application.run(host="0.0.0.0", port=8080, debug=True)
